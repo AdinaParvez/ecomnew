29f358adcb78c68c587e711c1c12f7d5b99b1d59
AdinaParvez <81682886+AdinaParvez@users.noreply.github.com>
2023-07-23 16:57:58 +0500

    updateserver1

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ const app=express();
 //middlewares
 app.use(cors())
 app.use(express.json())
-app.use(morgan('dev'))
+app.use(morgan('prod'))
 app.use(express.static(path.join(__dirname, './client/build')))
+
 //routes
 app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/category', categoryRoutes)
@@ -33,7 +34,8 @@ app.use('/api/v1/product', productRoutes)
 //rest api
 app.use('*',function(req,res){
 res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+});
+
 //PORT
 const PORT=process.env.PORT || 8080;
 
